dcd82a442e4d31af59ff922568fda5d0f9942bd7
fernando <fernando@juandemariana.org>
2024-11-04 23:53:17 +0100

    Create the structure of the Youtube components

---
+++ buey/components/youtube/ShowVideoYoutubeID.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="video-container">
+    <iframe 
+      :src="videoUrl" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+      allowfullscreen
+      class="video-iframe"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    video: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    videoUrl() {
+      return `https://www.youtube.com/embed/${this.video}`;
+    }
+  }
+}
+</script>
+
+<style>
+.video-container {
+  width: 100%;
+  position: relative;
+  padding-bottom: 56.25%; /* 16:9 Aspect Ratio */
+  height: 0;
+}
+
+.video-iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
+
---
+++ buey/components/youtube/ShowVideoYoutubeURL.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="video-container">
+    <iframe
+      :src="videoUrl"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    video: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    videoUrl() {
+      const videoId = this.video.split('v=')[1];
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.video-container {
+  position: relative;
+  width: 100%;
+  padding-bottom: 56.25%; /* 16:9 Aspect Ratio */
+  height: 0;
+}
+
+.video-container iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
+
