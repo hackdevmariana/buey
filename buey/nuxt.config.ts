65d922dcb2ac02ac3f00f075797223877cdc7279
fernando <fernando@juandemariana.org>
2024-11-05 00:04:44 +0100

    Config app with nuxt.config.tx and package-lock.json

--- buey/nuxt.config.ts
+++ buey/nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  compatibilityDate: '2024-11-04',
+
+  devtools: { enabled: true },
+  css: [
+    "bootstrap/dist/css/bootstrap.min.css",
+    '@/public/acssories.css',
+  ],
+  modules: ["@nuxt/ui", "@nuxt/image", 'nuxt-parallax', 'nuxt-aos'],
+
+
+
+  
+
 })
