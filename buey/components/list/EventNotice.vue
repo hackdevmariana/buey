bae39db04cd125ae86a62288a70c759c192063a8
fernando <fernando@juandemariana.org>
2024-11-05 23:22:50 +0100

    Add lists components

---
+++ buey/components/list/EventNotice.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="event-container">
+    <div class="icon-column">
+      <Icon name="ion:calendar-outline" class="icon" />
+    </div>
+    <div class="date-column">
+      <div class="day">{{ day }}</div>
+      <div class="month">{{ month }}</div>
+      <div class="year">{{ year }}</div>
+    </div>
+    <div class="details-column">
+      <div class="time">{{ startTime }} - {{ endingTime }}</div>
+      <div class="title"><a :href="link" class="event-link">{{ title }}</a></div>
+      <div class="text">{{ text }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    day: {
+      type: String,
+      default: ""
+    },
+    month: {
+      type: String,
+      default: ""
+    },
+    year: {
+      type: String,
+      default: ""
+    },
+    startTime: {
+      type: String,
+      default: ""
+    },
+    endingTime: {
+      type: String,
+      default: ""
+    },
+    title: {
+      type: String,
+      default: ""
+    },
+    text: {
+      type: String,
+      default: ""
+    },
+    link: {
+      type: String,
+      default: ""
+    }
+  }
+}
+</script>
+
+<style scoped>
+.event-container {
+  display: flex;
+  border: 1px solid #ccc;
+  padding: 10px;
+  width: 100%;
+  padding-top: 1rem;
+}
+
+.date-column {
+  flex: 1;
+  text-align: center;
+}
+
+.icon-column {
+  flex: 1;
+  text-align: center;
+  font-size: 4rem;
+}
+
+.day {
+  font-size: 3rem;
+  font-weight: bold;
+}
+
+.month,
+.year {
+  font-size: 16px;
+}
+
+.details-column {
+  flex: 6;
+  padding-left: 20px;
+}
+
+.time {
+  font-size: 14px;
+}
+
+.title {
+  font-size: 18px;
+  font-weight: bold;
+  margin: 5px 0;
+}
+
+.text {
+  font-size: 14px;
+}
+
+.event-link {
+  text-decoration: none;
+  color: inherit;
+  font-size: 2rem;
+}
+</style>
---
+++ buey/components/list/EventNoticeButton.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="container-event">
+    <div class="day">{{ day }}</div>
+    <div class="month-year">
+      <strong>{{ month }} {{ year }}</strong>
+      <div>{{ startTime }} - {{ endingTime }}</div>
+    </div>
+    <div class="column-image image">
+      <img :src="image" alt="Event image" class="rounded-image" />
+    </div>
+    <div class="column title-text">
+      <strong class="title">{{ title }}</strong>
+      <div class="text">{{ text }}</div>
+    </div>
+    <div class="button">
+      <a :href="linkButton" class="button">{{ textButton }}</a>
+    </div>
+    <div class="button-mobile">
+      <a :href="linkButton">
+      <Icon name="flowbite:angle-right-outline" class="icon" />
+    </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    day: String,
+    month: String,
+    year: String,
+    startTime: String,
+    endingTime: String,
+    image: String,
+    title: String,
+    text: String,
+    textButton: String,
+    linkButton: String,
+  },
+};
+</script>
+
+<style scoped>
+.container-event {
+  padding-top: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.column-image {
+  flex-basis: 10%;
+  display: block; /* Por defecto, se muestra en todas las pantallas */
+}
+.day {
+  font-size: 4em;
+  flex-basis: 10%;
+}
+.month-year strong {
+  font-weight: bold;
+  flex-basis: 20%;
+
+}
+.image .rounded-image {
+  border-radius: 50%;
+  max-width: 100px;
+}
+.title {
+  font-size: 1.5em;
+  font-weight: bold;
+}
+.text {
+  font-size: 0.875em;
+}
+.button, .button-mobile {
+  background-color: var(--color-bg-button);
+  color: var(--color-text-button);
+  padding: 10px 20px;
+  text-decoration: none;
+  border-radius: 5px;
+  flex-basis: 5%;
+
+}
+
+.button-mobile {
+  font-size: 2em;
+}
+
+.title-text {
+  flex-basis: 40%;
+}
+
+@media (max-width: 719px) {
+  .column-image {
+    display: none;
+  }
+  .button {
+    display: none;
+  }
+}
+
+@media (min-width: 719px) {
+  .button-mobile {
+    display: none;
+  }
+}
+</style>
