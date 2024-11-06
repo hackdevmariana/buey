d695f5a73cd653a61166df5bfa2fa1abc60634e4
fernando <fernando@juandemariana.org>
2024-11-06 23:58:00 +0100

    Add grids page

---
+++ buey/pages/grids.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup></script>
+
+<template>
+  
+  <div class="container">
+    <h1>Ejemplo de poster</h1>
+<GridData :items="products">
+    <template #default="{ item }">
+      <div class="product-card">
+        <img :src="item.image" alt="Product Image" />
+        <h4>{{ item.name }}</h4>
+        <p>{{ item.price | currency }}</p>
+      </div>
+    </template>
+  </GridData>
+  </div>
+  </template>
+  
