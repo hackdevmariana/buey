81ed36e9624931cf37968df57f27a41bd6d1c510
fernando <fernando@juandemariana.org>
2024-11-06 23:57:44 +0100

    Add effectsletters page

---
+++ buey/pages/effectsletters.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="container">
+    <h2>Propiedades animables en CSS</h2>
+    <ul>
+      <li>Propiedades de color: como color, background-color, border-color.</li>
+      <li>
+        Dimensiones: como width, height, top, right, bottom, left.</li>
+      <li>
+        Espaciado: como margin, padding.</li>
+      <li>
+        Bordes: como border-width, border-radius.</li>
+      <li>
+        Opacidad: opacity.</li>
+      <li>
+        Transformaciones: como transform (que incluye translate, rotate, scale, skew).</li>
+      <li>
+        Sombras: como box-shadow, text-shadow.</li>
+      <li>
+        Filtros: como filter (que incluye blur, brightness, contrast, drop-shadow, grayscale, hue-rotate, invert,
+        saturate, sepia).</li>
+      <li>
+        font-size: El tamaño de la fuente.</li>
+      <li>
+        font-weight: El grosor de la fuente.</li>
+      <li>
+        color: El color del texto.</li>
+      <li>
+        line-height: La altura de línea del texto.</li>
+      <li>
+        letter-spacing: El espacio entre caracteres.</li>
+      <li>
+        text-shadow: La sombra del texto.</li>
+      <li>
+        word-spacing: El espacio entre palabras.</li>
+
+    </ul>
+    <NuxtLink to="/testletreros">Pruebas de efectos con letras</NuxtLink>
+
+  </div>
+</template>
+
+<style scoped></style>
