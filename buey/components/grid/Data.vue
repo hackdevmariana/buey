7425ace144e004409467cd0bcb82a95ff748088c
fernando <fernando@juandemariana.org>
2024-11-06 23:52:38 +0100

    Add grid components structure

---
+++ buey/components/grid/Data.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="data-grid">
+      <div
+        v-for="(item, index) in items"
+        :key="index"
+        class="data-item"
+      >
+        <slot :item="item"></slot>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      items: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+  </script>
+  
