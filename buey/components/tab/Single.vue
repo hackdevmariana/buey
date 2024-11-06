4f41d876cd49f9ef0689e2cd9bb4a5e1f99d602e
fernando <fernando@juandemariana.org>
2024-11-06 23:53:54 +0100

    Add tabs components structure

---
+++ buey/components/tab/Single.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="tabs">
+      <div class="tab-header">
+        <button
+          v-for="(tab, index) in tabs"
+          :key="index"
+          :class="{ active: activeTab === index }"
+          @click="activeTab = index"
+        >
+          {{ tab.label }}
+        </button>
+      </div>
+      <div class="tab-content">
+        <slot :name="`tab-${activeTab}`"></slot>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      tabs: {
+        type: Array,
+        default: () => [{ label: 'Tab 1' }, { label: 'Tab 2' }]
+      }
+    },
+    data() {
+      return {
+        activeTab: 0
+      };
+    }
+  }
+  </script>
+  
