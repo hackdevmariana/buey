4f1419f0ef2582d837a4b03476a46dc698e6e5f9
fernando <fernando@juandemariana.org>
2024-11-06 23:53:05 +0100

    Add cards components structure

---
+++ buey/components/card/Single.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="card" :style="{ backgroundColor: backgroundColor }">
+      <div class="card-icon" v-if="icon">
+        <img :src="icon" alt="icon" />
+      </div>
+      <h2>{{ title }}</h2>
+      <p>{{ description }}</p>
+      <button v-if="ctaText" @click="onButtonClick">{{ ctaText }}</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      icon: String,
+      title: {
+        type: String,
+        default: 'Default Title'
+      },
+      description: {
+        type: String,
+        default: 'This is a default description.'
+      },
+      ctaText: String,
+      backgroundColor: {
+        type: String,
+        default: '#fff'
+      }
+    },
+    methods: {
+      onButtonClick() {
+        this.$emit('button-click');
+      }
+    }
+  }
+  </script>
+  
