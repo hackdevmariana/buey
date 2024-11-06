d5d5dedf18603e51cb8902fbc57d46d762399fb0
fernando <fernando@juandemariana.org>
2024-11-06 23:53:28 +0100

    Add modal components structure

---
+++ buey/components/modal/Single.vue
@@ -0,0 +1,22 @@
+<template>
+    <div v-if="visible" class="modal-backdrop" @click="close">
+      <div class="modal" @click.stop>
+        <button class="close-button" @click="close">✖</button>
+        <slot></slot>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      visible: Boolean
+    },
+    methods: {
+      close() {
+        this.$emit('close');
+      }
+    }
+  }
+  </script>
+  
