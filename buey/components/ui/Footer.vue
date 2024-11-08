349714689331e55352bbb818ff610a2630d517d3
fernando <fernando@juandemariana.org>
2024-11-08 16:26:51 +0100

    Add ui/Footer component structure

---
+++ buey/components/ui/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+    <footer class="footer">
+    <div class="container">
+    <div class="inline-components">
+    <LineCopyleft text="Yo mismo" link="https://google.es" />
+    <LineCopyleft text="Yo mismo" link="https://google.es" />
+    </div>
+    </div>
+    </footer>
+    </template>
+    
+    <script setup>
+    // Puedes incluir props o composición lógica aquí si fuera necesario
+    </script>
+    
+    <style scoped>
+    .footer {
+    width: 100%;
+    padding: 1rem;
+    background-color: #333;
+    color: white;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    justify-content: center; /* Centra los elementos horizontalmente */
+    align-items: center;
+    }
+    
+    .inline-components {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    }
+    
+    .container {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    }
+    </style>
