1c1757a4fabcb6545d931e5d5266d4673e52f1f2
fernando <fernando@juandemariana.org>
2024-11-11 00:30:26 +0100

    Add textmotion/ScaleOnLoad component

---
+++ buey/components/textmotion/ScaleOnLoad.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <p class="scale-text">{{ text }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    },
+    mounted() {
+        this.animateText();
+    },
+    methods: {
+        animateText() {
+            const textElement = this.$el.querySelector('.scale-text');
+            textElement.style.transform = 'scale(2.5)';
+            setTimeout(() => {
+                textElement.style.transform = 'scale(1)';
+            }, 100); // Delay to trigger the animation
+        }
+    }
+}
+</script>
+
+<style scoped>
+.scale-text {
+    transition: transform 1s ease-in-out;
+}
+</style>
