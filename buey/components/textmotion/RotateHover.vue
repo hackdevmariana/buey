b93eb0c50b0103dfc8fdd2cdd347ae1d8888e05c
fernando <fernando@juandemariana.org>
2024-11-11 00:13:32 +0100

    Add textmotion/RotateHover component

---
+++ buey/components/textmotion/RotateHover.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p class="rotate-text">{{ text }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.rotate-text {
+    transform: rotate(0deg);
+    transition: transform 0.5s ease-in-out;
+}
+
+.rotate-text:hover {
+    transform: rotate(360deg);
+}
+</style>
