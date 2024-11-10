978e2a2ac27cf0cc5c261905926eee450298432a
fernando <fernando@juandemariana.org>
2024-11-10 20:53:53 +0100

    Add textmotion/SizeHover component

---
+++ buey/components/textmotion/SizeHover.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p class="animated-text">{{ text }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.animated-text {
+    font-size: 1rem;
+    transition: font-size 0.5s ease-in-out;
+}
+
+.animated-text:hover {
+    font-size: 2rem;
+}
+</style>
