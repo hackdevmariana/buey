d76d6d0e89d04aa1969a9f73079d4a5ca2382373
fernando <fernando@juandemariana.org>
2024-11-10 23:55:58 +0100

    Add textmotion/TranslateHover component

---
+++ buey/components/textmotion/TranslateHover.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p class="translate-text">{{ text }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.translate-text {
+    transform: translateX(0);
+    transition: transform 0.5s ease-in-out;
+}
+
+.translate-text:hover {
+    transform: translateX(50px);
+}
+</style>
