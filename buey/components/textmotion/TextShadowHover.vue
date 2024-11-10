2e5cfec7c1e298bdb81c2552d875ae08c63d42a1
fernando <fernando@juandemariana.org>
2024-11-11 00:51:26 +0100

    Add textmotion/TextShadowHover component

---
+++ buey/components/textmotion/TextShadowHover.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p class="text-shadow">{{ text }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.text-shadow {
+    text-shadow: 0px 0px 0px rgba(0, 0, 0, 0);
+    transition: text-shadow 0.5s ease-in-out;
+}
+
+.text-shadow:hover {
+    text-shadow: 10px 10px 20px rgba(0, 0, 0, 0.9);
+}
+</style>
