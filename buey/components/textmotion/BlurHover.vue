e3f0e7b65a57aa590beedc165e782b01b82e1d36
fernando <fernando@juandemariana.org>
2024-11-10 21:10:10 +0100

    Add textmotion/BlurHover component

---
+++ buey/components/textmotion/BlurHover.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p class="blur-text">{{ text }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.blur-text {
+    filter: blur(5px);
+    transition: filter 0.5s ease-in-out;
+}
+
+.blur-text:hover {
+    filter: blur(0);
+}
+</style>
