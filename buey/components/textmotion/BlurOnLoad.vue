69cc77402d897e058f4daf54cd12d0533990f559
fernando <fernando@juandemariana.org>
2024-11-10 21:08:08 +0100

    Add textmotion/BlurOnLoad component

---
+++ buey/components/textmotion/BlurOnLoad.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <p class="blur-text">{{ text }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    },
+    mounted() {
+        this.animateText();
+    },
+    methods: {
+        animateText() {
+            const textElement = this.$el.querySelector('.blur-text');
+            textElement.style.filter = 'blur(100px)';
+            setTimeout(() => {
+                textElement.style.filter = 'blur(0)';
+            }, 100); // Delay to trigger the animation
+        }
+    }
+}
+</script>
+
+<style scoped>
+.blur-text {
+    transition: filter 1s ease-in-out;
+}
+</style>
