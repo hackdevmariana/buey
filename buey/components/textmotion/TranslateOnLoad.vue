5a58b5fc421e7d6509377ebf474559ffcd1e3c8d
fernando <fernando@juandemariana.org>
2024-11-10 23:50:51 +0100

    Add textmotion/TranslateOnLoad component

---
+++ buey/components/textmotion/TranslateOnLoad.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <p class="translate-text">{{ text }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    },
+    mounted() {
+        this.animateText();
+    },
+    methods: {
+        animateText() {
+            const textElement = this.$el.querySelector('.translate-text');
+            textElement.style.transform = 'translateX(500px)';
+            setTimeout(() => {
+                textElement.style.transform = 'translateX(0)';
+            }, 100); // Delay to trigger the animation
+        }
+    }
+}
+</script>
+
+<style scoped>
+.translate-text {
+    transition: transform 2s ease-in-out;
+}
+</style>
