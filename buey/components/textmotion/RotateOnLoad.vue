3abd8c87ea5c6e6a29498e024001f24ec73d86d1
fernando <fernando@juandemariana.org>
2024-11-11 00:13:57 +0100

    Add textmotion/RotateOnLoad component

---
+++ buey/components/textmotion/RotateOnLoad.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <p class="rotate-text">{{ text }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    },
+    mounted() {
+        this.animateText();
+    },
+    methods: {
+        animateText() {
+            const textElement = this.$el.querySelector('.rotate-text');
+            textElement.style.transform = 'rotate(0deg)';
+            setTimeout(() => {
+                textElement.style.transform = 'rotate(360deg)';
+            }, 100); // Delay to trigger the animation
+        }
+    }
+}
+</script>
+
+<style scoped>
+.rotate-text {
+    transition: transform 1s ease-in-out;
+}
+</style>
