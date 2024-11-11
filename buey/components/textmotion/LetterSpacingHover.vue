8fc76b65515c152a4d848cd7a868e2dabad291fd
fernando <fernando@juandemariana.org>
2024-11-11 23:18:35 +0100

    Add textmotion/LetterSpacingOnLoad and LetterSpacingHover

---
+++ buey/components/textmotion/LetterSpacingHover.vue
@@ -0,0 +1,36 @@
+<template>
+    <div :style="style" @mouseover="hover" @mouseleave="leave">{{ text }}</div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            default: 'Hola, mundo'
+        }
+    },
+    data() {
+        return {
+            style: {
+                transition: 'letter-spacing 0.5s ease',
+                letterSpacing: '0px'
+            }
+        };
+    },
+    methods: {
+        hover() {
+            this.style.letterSpacing = '5px';
+        },
+        leave() {
+            this.style.letterSpacing = '0px';
+        }
+    }
+};
+</script>
+
+<style scoped>
+div {
+    font-size: 24px;
+}
+</style>
---
+++ buey/components/textmotion/LetterSpacingOnLoad.vue
@@ -0,0 +1,31 @@
+<template>
+    <div :style="style">{{ text }}</div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            default: 'Hola, mundo'
+        }
+    },
+    data() {
+        return {
+            style: {
+                transition: 'letter-spacing 0.5s ease',
+                letterSpacing: '0px'
+            }
+        };
+    },
+    mounted() {
+        this.style.letterSpacing = '5px';
+    }
+};
+</script>
+
+<style scoped>
+div {
+    font-size: 24px;
+}
+</style>
