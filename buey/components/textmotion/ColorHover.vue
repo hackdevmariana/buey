eb4d052d4924b9e5dc106b876a08249278928032
fernando <fernando@juandemariana.org>
2024-11-11 23:19:16 +0100

    Add textmotion/ColorOnLoad and ColorHover components

---
+++ buey/components/textmotion/ColorHover.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <p class="color-text">{{ text }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true
+        },
+        startColor: {
+            type: String,
+            default: 'black'
+        },
+        hoverColor: {
+            type: String,
+            default: 'green'
+        }
+    }
+}
+</script>
+
+<style scoped>
+.color-text {
+    color: var(--start-color, black);
+    transition: color 0.5s ease-in-out;
+}
+
+.color-text:hover {
+    color: var(--hover-color, green);
+}
+</style>
---
+++ buey/components/textmotion/ColorOnLoad.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <p class="color-text">{{ text }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true
+        },
+        startColor: {
+            type: String,
+            default: 'red'
+        },
+        endColor: {
+            type: String,
+            default: 'blue'
+        }
+    },
+    mounted() {
+        this.animateText();
+    },
+    methods: {
+        animateText() {
+            const textElement = this.$el.querySelector('.color-text');
+            textElement.style.color = this.startColor;
+            setTimeout(() => {
+                textElement.style.color = this.endColor;
+            }, 100); // Delay to trigger the animation
+        }
+    }
+}
+</script>
+
+<style scoped>
+.color-text {
+    transition: color 1s ease-in-out;
+}
+</style>
