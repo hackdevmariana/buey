23e73635509d2fdff65e797a4b9eab04eb326ee9
fernando <fernando@juandemariana.org>
2024-11-11 23:28:58 +0100

    Add textmotion/BackgroundColorHover component

---
+++ buey/components/textmotion/BackgroundColorHover.vue
@@ -0,0 +1,45 @@
+<template>
+    <div :style="style" @mouseover="hover" @mouseleave="leave">{{ text }}</div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            default: 'Hola, mundo'
+        },
+        colorStart: {
+            type: String,
+            default: '#ffffff'
+        },
+        colorEnd: {
+            type: String,
+            default: '#ffcc00'
+        }
+    },
+    data() {
+        return {
+            style: {
+                transition: 'background-color 0.5s ease',
+                backgroundColor: this.colorStart
+            }
+        };
+    },
+    methods: {
+        hover() {
+            this.style.backgroundColor = this.colorEnd;
+        },
+        leave() {
+            this.style.backgroundColor = this.colorStart;
+        }
+    }
+};
+</script>
+
+<style scoped>
+div {
+    font-size: 24px;
+    padding: 10px;
+}
+</style>
