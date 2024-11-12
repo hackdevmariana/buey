e2b2108bd690b7fe47530ac64575bbec45b21060
fernando <fernando@juandemariana.org>
2024-11-12 23:58:28 +0100

    Add images/ZoomOpacity component

---
+++ buey/components/images/ZoomOpacity.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps({
+  imageSrc: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div class="image-container">
+    <img :src="imageSrc" alt="Image with Zoom and Opacity" class="image" />
+  </div>
+</template>
+
+<style scoped>
+.image-container {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transition: transform 0.3s ease, opacity 0.3s ease;
+}
+
+.image-container:hover .image {
+  transform: scale(1.1);
+  opacity: 0.8;
+}
+</style>
