319dfb79542299a638961d185874944c5df4ff32
fernando <fernando@juandemariana.org>
2024-11-12 23:56:22 +0100

    Add images/ColorHover component

---
+++ buey/components/images/ColorHover.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps({
+  imageSrc: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div class="image-container">
+    <img :src="imageSrc" alt="Image with Grayscale" class="image" />
+  </div>
+</template>
+
+<style scoped>
+.image-container {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  filter: grayscale(100%);
+  transition: filter 0.3s ease;
+}
+
+.image-container:hover .image {
+  filter: grayscale(0%);
+}
+</style>
