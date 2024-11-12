4c19f0126ef466dbd5c4394e697a2faf16185d10
fernando <fernando@juandemariana.org>
2024-11-12 23:57:51 +0100

    Add images/ZoomHover component

---
+++ buey/components/images/ZoomHover.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+  imageSrc: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div class="image-container">
+    <img :src="imageSrc" alt="Image with Zoom" class="image" />
+  </div>
+</template>
+
+<style scoped>
+.image-container {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transition: transform 0.3s ease;
+}
+
+.image-container:hover .image {
+  transform: scale(1.1);
+}
+</style>
