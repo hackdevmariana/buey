3c67fb67dc642d760a11f0c5d4867dfd7b3a6628
fernando <fernando@juandemariana.org>
2024-11-12 23:57:18 +0100

    Add images/BlurHover component

---
+++ buey/components/images/BlurHover.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+  imageSrc: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div class="image-container">
+    <img :src="imageSrc" alt="Image with Hover Blur" class="image" />
+  </div>
+</template>
+
+<style scoped>
+.image-container {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transition: filter 0.3s ease;
+}
+
+.image-container:hover .image {
+  filter: blur(5px);
+}
+</style>
